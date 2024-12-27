f234573fc76514649f54abcebe4eac058ba8f886
Z004RY7V <renjith.k@siemens.com>
2024-12-27 12:36:10 +0530

    comiting the chages related to lifecycle hooks

--- behind-scenes-01-starting-setup/app.js
+++ behind-scenes-01-starting-setup/app.js
@@ -5,17 +5,47 @@ const app = Vue.createApp({
       message: 'Vue is great!',
     };
   },
+  beforeCreate() {
+    console.log('beforeCreate');
+  },
+  created() {
+    console.log('created');
+  },
+  beforeMount() {
+    console.log('beforeMount');
+  },
+  mounted() {
+    console.log('mounted');
+  },
+  beforeUpdate() {
+    console.log('beforeUpdate');
+  },
+  updated() {
+    console.log('updated');
+  },
+  beforeUnmount() {
+    console.log('beforeUnmount');
+  },
+  unmounted() {
+    console.log('unmounted');
+  },
   methods: {
     saveInput(event) {
       this.currentUserInput = event.target.value;
     },
     setText() {
-      this.message = this.currentUserInput;
+      //this.message = this.currentUserInput;
+      this.message = this.$refs.userInput.value;
+      console.dir(this.$refs.userInput);
     },
   },
 });
 
 app.mount('#app');
+
+setTimeout(() => {
+  app.unmount();
+}, 3000);
 
 
 const app2 = Vue.createApp({
