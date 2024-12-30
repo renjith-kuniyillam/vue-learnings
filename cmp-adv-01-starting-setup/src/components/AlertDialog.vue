b952bad4fded30e303a4f854ff22f1e3cb01e5f7
Z004RY7V <renjith.k@siemens.com>
2024-12-30 12:05:33 +0530

    commiting the latest changes

---
+++ cmp-adv-01-starting-setup/src/components/AlertDialog.vue
@@ -0,0 +1,23 @@
+<template>
+    <dialog open>
+        <slot name="dialogContent"></slot>
+    </dialog>
+</template>
+<script>
+export default {
+    name: 'AlertDialog',
+    mounted: function() {
+        console.log(this.$slots);
+    },
+}
+</script>
+<style scoped>
+dialog {
+    width: 100%;
+    max-width: 20rem;
+    margin: 2rem auto;
+    border-radius: 12px;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+    padding: 1rem;
+}
+</style>
--- cmp-adv-01-starting-setup/src/components/ManageGoals.vue
+++ cmp-adv-01-starting-setup/src/components/ManageGoals.vue
@@ -1,4 +1,42 @@
 <template>
-    <h2>Manage Goals</h2>
-    <input type="text"/>
+  <h2>Manage Goals</h2>
+  <input ref="inputEle" type="text" />
+  <button @click="save">Save</button>
+  <teleport to="body">
+    <alert-dialog v-if="inputIsEmpty">
+      <template #dialogContent>
+        <h2>Alert!!</h2>
+        <p>Please enter a valid input</p>
+        <button @click="onConfirm">Ok</button>
+      </template>
+    </alert-dialog>
+  </teleport>
 </template>
+<script>
+import AlertDialog from "./AlertDialog.vue";
+
+export default {
+  components: {
+    AlertDialog,
+  },
+  name: "ManageGoals",
+  mounted: function () {
+    console.log(this.$slots);
+  },
+  data() {
+    return {
+      inputIsEmpty: false,
+    };
+  },
+  methods: {
+    save() {
+      if (!this.$refs.inputEle.value) {
+        this.inputIsEmpty = this.$refs.inputEle.value || true;
+      }
+    },
+    onConfirm() {
+      this.inputIsEmpty = false;
+    },
+  },
+};
+</script>
